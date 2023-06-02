e3a81daa616fc1fca0e25da88626ea2e479ff0fb
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-02 23:42:40 +0530

    update layout, values and build.gradle

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun backSpaceAction(view: View) {}
+    fun allClearAction(view: View) {}
+    fun equalsAction(view: View) {}
+    fun operationAction(view: View) {}
+    fun numberAction(view: View) {}
 }
