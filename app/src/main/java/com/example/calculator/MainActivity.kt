184f143f8c8e97ed48094447f48a9c7b3df914f4
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-06-02 23:59:09 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,16 +3,53 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var canAddOperation = false
+    private var canAddDecimal = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun backSpaceAction(view: View) {}
-    fun allClearAction(view: View) {}
+    fun backSpaceAction(view: View)
+    {
+        val length = workingsTV.length()
+        if (length > 0)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+    }
+    fun allClearAction(view: View)
+    {
+        workingsTV.text = ""
+        resultsTV.text = ""
+    }
+
     fun equalsAction(view: View) {}
-    fun operationAction(view: View) {}
-    fun numberAction(view: View) {}
+    fun operationAction(view: View)
+    {
+        if(view is Button && canAddOperation)
+        {
+            workingsTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+    fun numberAction(view: View)
+    {
+        if(view is Button)
+        {
+            if(view.text == ".")
+            {
+                if(canAddDecimal)
+                    workingsTV.append(view.text)
+                canAddDecimal = false
+            }
+            else
+                workingsTV.append(view.text)
+            canAddOperation = true
+        }
+    }
 }
